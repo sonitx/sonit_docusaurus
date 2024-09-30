3464e6651540f03d5e550743f906523452720644
Son Nguyen <son.nguyen.2@neyu.co>
2024-09-30 23:30:35 +0700

    add sitemap

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -49,21 +49,6 @@ const config = {
         containerId: 'GTM-PBLB5W9L',
       },
     ],
-    [
-      '@docusaurus/plugin-sitemap',
-      {
-        lastmod: 'date',
-        changefreq: 'weekly',
-        priority: 0.5,
-        ignorePatterns: ['/tags/**'],
-        filename: 'sitemap.xml',
-        createSitemapItems: async (params) => {
-          const {defaultCreateSitemapItems, ...rest} = params;
-          const items = await defaultCreateSitemapItems(rest);
-          return items.filter((item) => !item.url.includes('/page/'));
-        },
-      },
-    ],
   ],
 
   presets: [
